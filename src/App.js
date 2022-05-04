9ae65ac8196ba90cf648632b022d0600e6c66401
carlosAndress101 <carlos.hinestrozap@unac.edu.co>
2022-05-04 18:03:22 +0200

    commit me

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
+import SendEmail from './components/resetPassword';
 import './styles/styles.scss';
 const App = () => {
   
   return (
-    <div className=' ed-grid card'>
-      <form>
-        <h1 className='s-75 m-50 lg-40 s-to-left m-to-center'>Reset Your Password</h1>
-        <p className='s-75 m-50 lg-40 s-to-left m-to-center'>To reset your password, enter your email below and submit. <br />
-          An email will be sent to you with instructions about how to <br />
-          complete the process. </p>
-
-        <div className='s-75 m-50 lg-40 s-to-left m-to-center'>
-          <b>Email Address</b>
-          <input type="text"></input>
-        </div>
-        <br></br>
-        <div className='s-75 m-50 lg-40 s-to-left m-to-center'>
-          <a className="button big">Reset Password</a>
-        </div>
-      </form>
-    </div>
+    <>
+      <SendEmail/>
+    </>
   );
 }
 
 export default App;
 //yarn add ed-grid edteam-style-guides sass
+
---
+++ src/components/resetPassword.js
@@ -0,0 +1,24 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+const sendEmail = () => {
+    return (
+        <div className=' ed-grid card'>
+            <form>
+                <h1 className='s-75 m-50 lg-40 s-to-left m-to-center'>Reset Your Password</h1>
+                <p className='s-75 m-50 lg-40 s-to-left m-to-center'>To reset your password, enter your email below and submit. <br />
+                    An email will be sent to you with instructions about how to <br />
+                    complete the process. </p>
+
+                <div className='s-75 m-50 lg-40 s-to-left m-to-center'>
+                    <b>Email Address</b>
+                    <input type="text"></input>
+                </div>
+                <br></br>
+                <div className='s-75 m-50 lg-40 s-to-left m-to-center'>
+                    <a className="button big">Reset Password</a>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default sendEmail;
