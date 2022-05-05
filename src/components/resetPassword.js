e7b250f6e1a1f5f66ff0a92ff00a86e3dfeeef33
carlos.hinestrozap <https://github.com/CarlosAndress/Project_Frontend.git>
2022-05-04 22:57:53 -0500

    commit me

--- src/components/resetPassword.js
+++ src/components/resetPassword.js
@@ -1,8 +1,10 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-const sendEmail = () => {
+import PropTypes from 'prop-types'
+
+const sendEmail = ({ handleSubmit, ...props }) => {
     return (
         <div className=' ed-grid card'>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <h1 className='s-75 m-50 lg-40 s-to-left m-to-center'>Reset Your Password</h1>
                 <p className='s-75 m-50 lg-40 s-to-left m-to-center'>To reset your password, enter your email below and submit. <br />
                     An email will be sent to you with instructions about how to <br />
@@ -10,7 +12,12 @@ const sendEmail = () => {
 
                 <div className='s-75 m-50 lg-40 s-to-left m-to-center'>
                     <b>Email Address</b>
-                    <input type="text"></input>
+                    <input
+                        type="text"
+                        value={props.userEmail}
+                        name="userEmail"
+                        placeholder='write your email'
+                    ></input>
                 </div>
                 <br></br>
                 <div className='s-75 m-50 lg-40 s-to-left m-to-center'>
@@ -20,5 +27,8 @@ const sendEmail = () => {
         </div>
     )
 }
-
+sendEmail.propTypes = {
+    handleSubmit: PropTypes.func.isRequired,
+    userEmail: PropTypes.string.isRequired,
+}
 export default sendEmail;
